1376ba61ef9634b612e85655a7bc71c629ed0485
sudiartakadek <sudiartakadek@ymail.com>
2026-05-05 16:08:33 +0800

    add sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,10 +7,12 @@ import tailwindcss from "@tailwindcss/vite";
 
 import cloudflare from "@astrojs/cloudflare";
 
+import sitemap from "@astrojs/sitemap";
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://sarilabstudio.com",
-  integrations: [react()],
+  integrations: [react(), sitemap()],
 
   vite: {
     plugins: [tailwindcss()],
